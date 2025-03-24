de637bd2ca8780f447cb0126705db1d4ba5d7d8a
Dan Frakes <dan@nodaintelligence.ai>
2025-03-24 16:16:06 -0400

    Use VM host IP as backup for PX4_CLIENT_HOST

--- edit_rcS.bash
+++ edit_rcS.bash
@@ -23,8 +23,13 @@ function get_vm_host_ip {
 function get_px4_client_ip {
     # Generates an IPv4 address (172.19.0.1) which points to the PX4 client Docker container
     # TODO investigate more robust networking options: if PX4 client container restarts, the IP will change, requiring this simulator to obtain the new IP and restart the mavsdk server
-    PX4_CLIENT_HOST=${PX4_CLIENT_HOST:-"px4-client"}
-    echo "$(dig $PX4_CLIENT_HOST +short)"
+    # If PX4_CLIENT_HOST is set, use it
+    # If not, use 127.0.0.1
+    if [ -n "$PX4_CLIENT_HOST" ]; then
+        echo "$(dig $PX4_CLIENT_HOST +short)"
+    else
+        get_vm_host_ip
+    fi
 }
 
 function get_host_ip {
@@ -48,8 +53,8 @@ fi
 
 CONFIG_FILE=${FIRMWARE_DIR}/build/etc/init.d-posix/px4-rc.mavlink
 
-echo "MAVSDK access from ${API_PARAM}"
 echo "QGroundControl access from ${QGC_PARAM}"
+echo "MAVSDK access from ${API_PARAM}"
 
 sed -i "s/mavlink start \-x \-u \$udp_gcs_port_local -r 4000000/mavlink start -x -u \$udp_gcs_port_local -r 4000000 ${QGC_PARAM}/" ${CONFIG_FILE}
 sed -i "s/mavlink start \-x \-u \$udp_offboard_port_local -r 4000000/mavlink start -x -u \$udp_offboard_port_local -r 4000000 ${API_PARAM}/" ${CONFIG_FILE}
